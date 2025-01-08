7ea95122eb06461d9909188d4b78eb654c4ba0de
tyrnor <carlosyanez997@gmail.com>
2025-01-08 14:47:30 +0100

    Add inline markdown module

---
+++ src/inline_markdown.py
@@ -0,0 +1,34 @@
+import re
+
+from textnode import TextNode,TextType
+
+def split_nodes_delimiter(old_nodes, delimeter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        split_nodes = []
+        sections = node.text.split(delimeter)
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+
+def extract_markdown_images(text):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+
+def extract_markdown_links(text):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
--- src/textnode.py
+++ src/textnode.py
@@ -42,3 +42,4 @@ def text_node_to_html_node(text_node):
             return LeafNode(tag= "img", value="", props= {"src": text_node.url, "alt" : text_node.text})
         case _:
             raise ValueError(f"Invalid text type: {text_node.text_type}")
+        

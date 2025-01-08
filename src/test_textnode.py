c4812dc19aba0d2a3d99c7a807b4e24c0579cd8a
tyrnor <carlosyanez997@gmail.com>
2025-01-08 14:22:56 +0100

    Add text node to html function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -23,6 +23,13 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("Text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "Text node")
         
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -24,3 +25,20 @@ class TextNode():
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    match(text_node.text_type):
+        case TextType.TEXT:
+            return LeafNode(tag= None, value= text_node.text)
+        case TextType.BOLD:
+            return LeafNode(tag= "b", value= text_node.text)
+        case TextType.ITALIC:
+            return LeafNode(tag= "i", value= text_node.text)
+        case TextType.CODE:
+            return LeafNode(tag= "code", value= text_node.text)
+        case TextType.LINK:
+            return LeafNode(tag= "a", value= text_node.text, props= {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode(tag= "img", value="", props= {"src": text_node.url, "alt" : text_node.text})
+        case _:
+            raise ValueError(f"Invalid text type: {text_node.text_type}")
